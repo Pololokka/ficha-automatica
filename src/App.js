9f39920f39db121a4c49e580c06225c009110b45
Pololokka <vitorbulbovas1@hotmail.com>
2023-02-21 18:34:45 -0300

    :sparkles: feat:adicionada função de trocar de foto e seção de informações gerais

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import PrintaImagem from './components/image/Image';
+import Informacoes from './components/info geral/InfoGeral';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <PrintaImagem />
+      <Informacoes />
       <Footer />
     </div>
   );
---
+++ src/components/image/Image.js
@@ -0,0 +1,24 @@
+import './Image.css'
+import { useState } from 'react';
+
+const PrintaImagem = () => {
+    const [imagemPlayer, setNovaImagem] = useState("https://cdn.discordapp.com/attachments/621499803884584998/1077674092137812078/TheTemplar.jpg")
+
+    const mudaFoto = () => {
+        const foto = document.querySelector("[data-foto]").value;
+        setNovaImagem(foto);
+    }
+    
+    return (
+        <div className="ficha__container">
+            <div className='interacao__container'>
+                <label htmlFor="link-foto" className='texto'>Link da sua foto:</label>
+                <input type="text" name="link-foto" className='texto foto__input' id="linkFoto" data-foto />
+                <button onClick={mudaFoto} className="texto botao__foto">Carregar</button>
+            </div>
+            <img src={imagemPlayer} alt="Imagem do personagem" className='imagem__container'/>
+        </div>
+    );
+}
+
+export default PrintaImagem
