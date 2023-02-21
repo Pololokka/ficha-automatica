d5980dcc4075b1986a04cfcd19035fd2bc63f0a5
Pololokka <vitorbulbovas1@hotmail.com>
2023-02-21 14:47:25 -0300

    :tada: Commit inicial

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import './css/Footer.css'
+
+function Footer() {
+    return (
+        <footer class="rodape--container">
+            <p class="subtitulo rodape__texto">Site desenvolvido por Vitor Bulbovas</p>
+            <p class="subtitulo rodape__texto">Me encontre nas redes sociais!</p>
+            <a href="https://www.instagram.com/vitorbulbovas/" target="_blank" rel="noreferrer" class="texto rodape--link rodape__texto">Instagram</a>
+            <a href="https://github.com/Pololokka" target="_blank" rel="noreferrer" class="texto rodape--link rodape__texto">Git</a>
+            <a href="https://www.linkedin.com/in/vitor-bulbovas-852348187/" target="_blank" rel="noreferrer" class="texto rodape--link rodape__texto">Linkedin</a>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import './css/Header.css'
+
+function Header() {
+    return (
+        <header class="cabecalho">
+            <h1 class="titulo cabecalho__titulo">Ficha Automática</h1>
+        </header>
+    );
+}
+
+export default Header
+  
