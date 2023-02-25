6f57a4e3fb23297170c8c1d9aab737ce81961740
Pololokka <vitorbulbovas1@hotmail.com>
2023-02-25 12:54:06 -0300

    :sparkles: feat:adicionada funcionalidade de remover item

--- src/components/equipamento/Equipamento.jsx
+++ src/components/equipamento/Equipamento.jsx
@@ -2,13 +2,14 @@ import './Equipamento.css'
 
 const Equipamento = () => {
     let count = 0;
+    let id = 0;
     
     const adicionaItem = () => {
         const equipLista = document.querySelector("[data-equip]");
         
         if(count < 10) {
             equipLista.innerHTML += `
-                    <li>
+                    <li id="${id}">
                         <label htmlFor="item">Item:</label>
                         <input type="text" class='geral__input' name="item" />
 
@@ -17,12 +18,27 @@ const Equipamento = () => {
 
                         <label htmlFor="peso">Peso:</label>
                         <input type="text" class='geral__input input__equip' name="peso" />
+
+                        <button class="texto botao__geral" data-remove="${id}">Remover</button>
                     </li>
                 `
              count++;
+             id++;
         }
-        
+
+        const botoes = document.querySelectorAll("[data-remove]");
+        botoes.forEach( elemento => {
+            elemento.addEventListener('click', evento => {
+                const achaID = evento.target.getAttribute('data-remove');
+                const itemRemover = document.getElementById(achaID);
+                itemRemover.remove();
+                count--;
+                console.log(itemRemover);
+            })
+        })
     }
+
+
     
     return (
         <div className="equipamento__container">
